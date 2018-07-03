15400b7fbd7492210f24d47295ad3877e7fdae95
Nico <nicolasbrailo@gmail.com>
2018-07-03 21:02:25 +0200

    Key drawing from multitouch events

--- app/src/main/java/com/nicobrailo/pianoli/DrawingCanvas.java
+++ app/src/main/java/com/nicobrailo/pianoli/DrawingCanvas.java
@@ -19,9 +19,8 @@ import java.util.Map;
 class DrawingCanvas extends SurfaceView implements SurfaceHolder.Callback {
 
     private final Point screen_size = new Point();
-
-    Integer current_key_press = null;
-
+    final int keys_count;
+    boolean key_pressed[];
     final int KEYS_WIDTH = 220;
 
     final int[][] KEY_COLORS = new int[][]{
@@ -34,8 +33,7 @@ class DrawingCanvas extends SurfaceView implements SurfaceHolder.Callback {
             {255,   0 ,  0},    // Red
     };
 
-    void draw_keys(Canvas canvas) {
-
+    void draw_all_keys(final Canvas canvas) {
         /* Reset canvas */ {
             Paint p = new Paint();
             p.setColor(Color.BLACK);
@@ -43,21 +41,26 @@ class DrawingCanvas extends SurfaceView implements SurfaceHolder.Callback {
         }
 
         // Round up (draw half a key if needed)
-        final int keys_count = 1 + (screen_size.x / KEYS_WIDTH);
         for (int i=0; i < keys_count; ++i) {
-            Rect r = new Rect();
-            r.left = i * KEYS_WIDTH;
-            r.right = r.left + KEYS_WIDTH;
-            r.top = 0;
-            r.bottom = screen_size.y;
-
-            final int col_idx = i % KEY_COLORS.length;
-            Paint p = new Paint();
-            final int alpha = (current_key_press != null && current_key_press == i)? 128 : 255;
-            p.setARGB(alpha, KEY_COLORS[col_idx][0], KEY_COLORS[col_idx][1], KEY_COLORS[col_idx][2]);
-
-            canvas.drawRect(r, p);
+            draw_key(canvas, i, key_pressed[i]);
         }
+    }
+
+    void draw_key(Canvas canvas, int key_idx, boolean pressed) {
+        Rect r = new Rect();
+        r.left = key_idx * KEYS_WIDTH;
+        r.right = r.left + KEYS_WIDTH;
+        r.top = 0;
+        r.bottom = screen_size.y;
+
+        final int col_idx = key_idx % KEY_COLORS.length;
+        Paint p = new Paint();
+        final int d = (pressed)? 50 : 0;
+        p.setARGB(255, Math.max(KEY_COLORS[col_idx][0] - d, 0),
+                          Math.max(KEY_COLORS[col_idx][1] - d, 0),
+                          Math.max(KEY_COLORS[col_idx][2] - d, 0));
+
+        canvas.drawRect(r, p);
     }
 
     public DrawingCanvas(AppCompatActivity context) {
@@ -67,13 +70,13 @@ class DrawingCanvas extends SurfaceView implements SurfaceHolder.Callback {
 
         Display display = context.getWindowManager().getDefaultDisplay();
         display.getSize(screen_size);
-        Log.d("XXXXXXXXX", "Display is " + screen_size.x + "x" + screen_size.y);
-    }
+        keys_count = 1 + (screen_size.x / KEYS_WIDTH);
 
-    @Override
-    public void draw(Canvas canvas)  {
-        super.draw(canvas);
-        Log.d("XXXXXXXXX", "DRAW");
+        key_pressed = new boolean[keys_count];
+        for (int i=0; i < key_pressed.length; ++i) key_pressed[i] = false;
+
+        Log.d("XXXXXXXXX", "Display is " + screen_size.x + "x" + screen_size.y +
+                                      ", there are " + keys_count + " keys");
     }
 
     @Override
@@ -81,22 +84,40 @@ class DrawingCanvas extends SurfaceView implements SurfaceHolder.Callback {
         redraw(surfaceHolder);
     }
 
-    private void redraw() {
+    public void redraw() {
         redraw(getHolder());
     }
 
     public void redraw(SurfaceHolder surfaceHolder) {
         Canvas canvas = surfaceHolder.lockCanvas();
-        draw_keys(canvas);
+        draw_all_keys(canvas);
         surfaceHolder.unlockCanvasAndPost(canvas);
     }
 
     void on_key_up(int key_idx) {
         Log.d("XXXXXXXXX", "Key " + key_idx + " is now UP");
+
+        if (key_idx > keys_count) {
+            // throw new Exception("");
+            // XXX TODO
+            Log.e("XXXXXXXXX", "Bad things happened");
+        }
+
+        key_pressed[key_idx] = false;
+        redraw();
     }
 
     void on_key_down(int key_idx) {
         Log.d("XXXXXXXXX", "Key " + key_idx + " is now DOWN");
+
+        if (key_idx > keys_count) {
+            // throw new Exception("");
+            // XXX TODO
+            Log.e("XXXXXXXXX", "Bad things happened");
+        }
+
+        key_pressed[key_idx] = true;
+        redraw();
     }
 
     Map<Integer, Integer> touch_pointer_to_keys = new HashMap<>();
@@ -131,8 +152,10 @@ class DrawingCanvas extends SurfaceView implements SurfaceHolder.Callback {
 
                 // check if key changed
                 if (touch_pointer_to_keys.get(ptr_id) != key_idx) {
-                    touch_pointer_to_keys.put(ptr_id, key_idx);
+                    Log.d("XXXXXXXXX", "Moved to another key");
+                    // Release key before storing new key_idx for new key down
                     on_key_up(touch_pointer_to_keys.get(ptr_id));
+                    touch_pointer_to_keys.put(ptr_id, key_idx);
                     on_key_down(key_idx);
                 }
 
