36dcfc1ffc37f6e08666a81a32bf9549b990f5e7
Nico <nicolasbrailo@gmail.com>
2018-07-22 11:56:24 +0200

    Adding sound resources

---
+++ app/src/main/res/raw/wget_notes.sh
@@ -0,0 +1,25 @@
+# notes from https://freesound.org/people/jobro/packs/2489/?page=1#sound
+# no_note from https://freesound.org/people/waveplay_old/sounds/221365/ 
+
+wget -O n01.mp3 https://freesound.org/data/previews/39/39187_35187-lq.mp3
+wget -O n02.mp3 https://freesound.org/data/previews/39/39188_35187-lq.mp3
+wget -O n03.mp3 https://freesound.org/data/previews/39/39189_35187-lq.mp3
+wget -O n04.mp3 https://freesound.org/data/previews/39/39190_35187-lq.mp3
+wget -O n05.mp3 https://freesound.org/data/previews/39/39191_35187-lq.mp3
+wget -O n06.mp3 https://freesound.org/data/previews/39/39193_35187-lq.mp3
+wget -O n07.mp3 https://freesound.org/data/previews/39/39194_35187-lq.mp3
+wget -O n08.mp3 https://freesound.org/data/previews/39/39195_35187-lq.mp3
+wget -O n09.mp3 https://freesound.org/data/previews/39/39196_35187-lq.mp3
+wget -O n10.mp3 https://freesound.org/data/previews/39/39197_35187-lq.mp3
+wget -O n11.mp3 https://freesound.org/data/previews/39/39198_35187-lq.mp3
+wget -O n12.mp3 https://freesound.org/data/previews/39/39199_35187-lq.mp3
+wget -O n13.mp3 https://freesound.org/data/previews/39/39200_35187-lq.mp3
+wget -O n14.mp3 https://freesound.org/data/previews/39/39201_35187-lq.mp3
+wget -O n15.mp3 https://freesound.org/data/previews/39/39202_35187-lq.mp3
+wget -O n16.mp3 https://freesound.org/data/previews/39/39203_35187-lq.mp3
+wget -O n17.mp3 https://freesound.org/data/previews/39/39204_35187-lq.mp3
+wget -O n18.mp3 https://freesound.org/data/previews/39/39205_35187-lq.mp3
+wget -O n19.mp3 https://freesound.org/data/previews/39/39206_35187-lq.mp3
+wget -O n20.mp3 https://freesound.org/data/previews/39/39207_35187-lq.mp3
+wget -O no_note.mp3 https://freesound.org/data/previews/221/221365_1676145-lq.mp3
+
