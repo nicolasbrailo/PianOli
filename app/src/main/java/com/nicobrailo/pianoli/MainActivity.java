2e05d759ad8c17040d0cb3a03ceb2640b7655e2a
Nico <nicolasbrailo@gmail.com>
2018-12-08 15:39:01 +0100

    Make full-screening more resilient in case of unsupported features

--- app/src/main/java/com/nicobrailo/pianoli/MainActivity.java
+++ app/src/main/java/com/nicobrailo/pianoli/MainActivity.java
@@ -17,19 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set fullscreen
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // Set a bunch of flags to make it full screen. If any of the features are
+        // not available, ignore them
+
+        try {
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        } catch (Exception e) {}
+
+        try {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } catch (Exception e) {}
+
+        try {
+            getSupportActionBar().hide();
+        } catch (Exception e) {}
+
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        } catch (Exception e) {}
 
         setContentView(R.layout.activity_main);
 
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
+        try {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        } catch (Exception e) {}
 
         lock_app();
     }
