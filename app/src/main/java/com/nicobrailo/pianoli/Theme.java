b742d324f952d59b13ac5f8601ba026959eef255
Peter Serwylo <peter@serwylo.com>
2023-09-08 21:37:44 +1000

    Refactor theme to make colours easier to specify.
    Previously the main colour and the pressed colour were
    defined far apart from eachother. Now they are coupled
    together into the KeyColour class.

--- app/src/main/java/com/nicobrailo/pianoli/Theme.java
+++ app/src/main/java/com/nicobrailo/pianoli/Theme.java
@@ -7,8 +7,7 @@ import androidx.core.graphics.ColorUtils;
 
 public class Theme {
 
-    private final int[] colours;
-    private final int[] pressedColours;
+    private final KeyColour[] colours;
 
     public static Theme fromPreferences(Context context) {
         String selectedTheme = Preferences.selectedTheme(context);
@@ -21,46 +20,54 @@ public class Theme {
         }
     }
 
-    private static final int[] BOOMWHACKER_COLORS = new int[] {
-            Color.rgb(220, 0, 0),     // Red
-            Color.rgb(255, 135, 0),   // Orange
-            Color.rgb(255, 255, 0),   // Yellow
-            Color.rgb(80, 220, 20),   // Light Green
-            Color.rgb(0, 150, 150),   // Dark Green
-            Color.rgb(95, 70, 165),   // Purple
-            Color.rgb(213, 43, 149),  // Pink
-    };
-
     /**
      * Note that light green, orange and yellow have higher lightness than other colours,
      * so adding just a little white doesn't have the desired effect.
      * That is why they have a larger proportion of white added in.
      */
-    private static final int[] BOOMWHACKER_PRESSED_COLORS = new int[] {
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[0], Color.WHITE, 0.5f),    // Red
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[1], Color.WHITE, 0.6f),    // Orange
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[2], Color.WHITE, 0.75f),   // Yellow
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[3], Color.WHITE, 0.6f),    // Light Green
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[4], Color.WHITE, 0.5f),    // Dark Green
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[5], Color.WHITE, 0.5f),    // Purple
-        ColorUtils.blendARGB(BOOMWHACKER_COLORS[6], Color.WHITE, 0.5f),    // Pink
-    };
-
-    private static final Theme BOOMWHACKER = new Theme(BOOMWHACKER_COLORS, BOOMWHACKER_PRESSED_COLORS);
-
-    private static final Theme BLACK_AND_WHITE = new Theme(
-            new int[] { Color.rgb(240, 240, 240) },
-            new int[] { Color.rgb(200, 200, 200) }
+    private static final Theme BOOMWHACKER = new Theme(
+            new KeyColour[] {
+                    KeyColour.createLighterWhenPressed(Color.rgb(220, 0, 0), 0.5f),     // Red
+                    KeyColour.createLighterWhenPressed(Color.rgb(255, 135, 0), 0.6f),   // Orange
+                    KeyColour.createLighterWhenPressed(Color.rgb(255, 255, 0), 0.75f),   // Yellow
+                    KeyColour.createLighterWhenPressed(Color.rgb(80, 220, 20), 0.6f),   // Light Green
+                    KeyColour.createLighterWhenPressed(Color.rgb(0, 150, 150), 0.5f),   // Dark Green
+                    KeyColour.createLighterWhenPressed(Color.rgb(95, 70, 165), 0.5f),   // Purple
+                    KeyColour.createLighterWhenPressed(Color.rgb(213, 43, 149), 0.5f),  // Pink
+            }
     );
 
-    private Theme(int[] colors, int[] pressedColors) {
+    private static final Theme BLACK_AND_WHITE = new Theme(
+            new KeyColour[] {
+                    new KeyColour(
+                            Color.rgb(240, 240, 240),
+                            Color.rgb(200, 200, 200)
+                    )
+            }
+    );
+
+    private Theme(KeyColour[] colors) {
         this.colours = colors;
-        this.pressedColours = pressedColors;
     }
 
     public int getColorForKey(int keyIndex, boolean isPressed) {
         final int col_idx = (keyIndex / 2) % colours.length;
-        return isPressed ? pressedColours[col_idx] : colours[col_idx];
+        final KeyColour key = colours[col_idx];
+        return isPressed ? key.pressedColour : key.colour;
+    }
+
+    private static class KeyColour {
+        public final int colour;
+        public final int pressedColour;
+
+        public KeyColour(int colour, int pressedColour) {
+            this.colour = colour;
+            this.pressedColour = pressedColour;
+        }
+
+        public static KeyColour createLighterWhenPressed(int colour, float blendWhiteFactor) {
+            return new KeyColour(colour, ColorUtils.blendARGB(colour, Color.WHITE, blendWhiteFactor));
+        }
     }
 
 }
